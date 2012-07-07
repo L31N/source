fed7bf192435e0733576f5de0677c083a15ab4cf
Daniel Belles <daniel.belles@web.de>
2012-07-07 12:24:58 +0200

    worked ahead on some bluetooth files

--- beagleboard/bluetooth/rfcommServer2.c
+++ beagleboard/bluetooth/rfcommServer2.c
@@ -6,31 +6,32 @@
 
 int main()
 {
-    printf("starting rfcommServer ...");
+    printf("starting rfcommServer ...\n");
 
     struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
     char buf[1024] = { 0 };
     int s, client, bytes_read;
     socklen_t opt = sizeof(rem_addr);
 
+
     // allocate socket
-    printf("allocate socket ...");
+    printf("allocate socket ...\n");
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
     // bind socket to port 1 of the first available 
     // local bluetooth adapter
-    printf("bind socket to first available bluetooth adapter ...");
+    printf("bind socket to first available bluetooth adapter ...\n");
     loc_addr.rc_family = AF_BLUETOOTH;
     loc_addr.rc_bdaddr = *BDADDR_ANY;
     loc_addr.rc_channel = (uint8_t) 1;
     bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 
     // put socket into listening mode
-    printf("put socket into listening mode ...");
+    printf("put socket into listening mode ...\n");
     listen(s, 1);
 
     // accept one connection
-    printf("accept connections ...");
+    printf("accept connections ...\n");
     client = accept(s, (struct sockaddr *)&rem_addr, &opt);
 
     ba2str( &rem_addr.rc_bdaddr, buf );
@@ -38,15 +39,16 @@ int main()
     memset(buf, 0, sizeof(buf));
 
     // read data from the client
-    printf("read data form the client ...");
+    printf("read data form the client ...\n");
     bytes_read = read(client, buf, sizeof(buf));
     if( bytes_read > 0 ) {
         printf("received [%s]\n", buf);
     }
 
     // close connection
-    printf("close connection ...");
+    printf("close connection ...\n");
     close(client);
     close(s);
+
     return 0;
 }
