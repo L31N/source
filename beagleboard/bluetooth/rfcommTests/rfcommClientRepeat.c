294e2c9143bb766ed319eafb3e9973b757f32f63
Daniel Belles <daniel.belles@web.de>
2012-07-08 22:13:17 +0200

    moved rfcommTestfiles into subdir rfcommTests ...

---
+++ beagleboard/bluetooth/rfcommTests/rfcommClientRepeat.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    while(1) {
+
+    struct sockaddr_rc addr = { 0 };
+    int s, status;
+    char* dest = *(argv+1);
+
+    // allocate a socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    // set the connection parameters (who to connect to)
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = (uint8_t) 1;
+    str2ba( dest, &addr.rc_bdaddr );
+
+    // connect to server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+    // send a message
+	if( status == 0 ) {
+        	//while(1) {
+			status = write(s, "hello!", 6);
+		//}
+    	}
+	else if (status < 0) perror("send data failed !\n");
+
+    //if( status < 0 ) perror("uh oh");
+
+    close(s);
+
+    } // end of while
+
+    usleep(100000);
+ //   } // end of while
+    return 0;
+}
+
---
+++ beagleboard/bluetooth/rfcommTests/rfcommServer2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+
+int main()
+{
+    printf("starting rfcommServer ...\n");
+
+    struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
+    char buf[1024] = { 0 };
+    int s, client, bytes_read;
+    socklen_t opt = sizeof(rem_addr);
+
+
+    // allocate socket
+    printf("allocate socket ...\n");
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    // bind socket to port 1 of the first available 
+    // local bluetooth adapter
+    printf("bind socket to first available bluetooth adapter ...\n");
+    loc_addr.rc_family = AF_BLUETOOTH;
+    loc_addr.rc_bdaddr = *BDADDR_ANY;
+    loc_addr.rc_channel = (uint8_t) 1;
+    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+
+    // put socket into listening mode
+    printf("put socket into listening mode ...\n");
+    listen(s, 1);
+
+    // accept one connection
+    printf("accept connections ...\n");
+    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+
+    ba2str( &rem_addr.rc_bdaddr, buf );
+    fprintf(stderr, "accepted connection from %s\n", buf);
+    memset(buf, 0, sizeof(buf));
+
+    // read data from the client
+    printf("read data form the client ...\n");
+    bytes_read = read(client, buf, sizeof(buf));
+    if( bytes_read > 0 ) {
+        printf("received [%s]\n", buf);
+    }
+
+    // close connection
+    printf("close connection ...\n");
+    close(client);
+    close(s);
+
+    return 0;
+}
