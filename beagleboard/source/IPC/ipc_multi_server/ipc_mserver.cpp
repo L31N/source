3fdb49ad692b01bc6ffee8f7385ab7882601c314
Daniel Belles <daniel.belles@web.de>
2012-08-29 21:55:42 +0200

    added extended the project "source/beagleboard/source/IPC/ipc_multi_server", it is now able to connect to multiple clients. (at least in theory) --> it gets an "segmentation fault when it recives a connection.)

--- beagleboard/source/IPC/ipc_multi_server/ipc_mserver.cpp
+++ beagleboard/source/IPC/ipc_multi_server/ipc_mserver.cpp
@@ -3,53 +3,101 @@
 #include <sys/types.h>
 #include <sys/un.h>
 
+#include <sys/select.h>
+
 #include <iostream>
 #include <string>
 
 #include <stdio.h>
 
-//using namespace std;
+using namespace std;
 
-const string SOCKET_FILE = "/tmp/ipc_mserver.uds";
+const int max_connections = 5;
+
+const string SOCKET_FILES [max_connections] = {
+    "/tmp/ipc_testing/ipc_mserver_00.uds",
+    "/tmp/ipc_testing/ipc_mserver_01.uds",
+    "/tmp/ipc_testing/ipc_mserver_02.uds",
+    "/tmp/ipc_testing/ipc_mserver_03.uds",
+    "/tmp/ipc_testing/ipc_mserver_04.uds"
+};
 
 int main () {
-    int sock, msgsock, rval;
-    struct sockaddr_un server;
+    int sockets[max_connections];
+    int max_sock = -1;
+
+    int active_socket, msgsock, rval;
+
+    struct sockaddr_un addresses[max_connections];
     char buf[1024];
 
-    sock = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("function socket()");
-        return -1;
+    fd_set* readable_sockets = new fd_set;
+    fd_set* writeable_sockets = new fd_set;
+
+    FD_ZERO(readable_sockets);
+    FD_ZERO(writeable_sockets);
+
+    for (int i = 0; i < max_connections; i++) {
+
+        addresses[i].sun_family = AF_UNIX;
+        strcpy(addresses[i].sun_path, SOCKET_FILES[i].c_str());
+
+        unlink(SOCKET_FILES[i].c_str());
+
+        sockets[i] = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (sockets[i] < 0) {
+            perror ("function socket()");
+            return -1;
+        }
+
+        if (bind(sockets[i], (struct sockaddr*)&addresses[i], sizeof(struct sockaddr_un)) < 0) {
+            perror("function bind()");
+            return -1;
+        }
+
+        if (listen(sockets[i], 1) == -1) {
+            perror("function listen(): ");
+            return -1;
+        }
+
+        cout << "socket_path -->" << addresses[i].sun_path << endl;
+
+        FD_SET(sockets[i], readable_sockets);
+        if (sockets[i] > max_sock) max_sock = sockets[i];
     }
 
-    server.sun_family = AF_UNIX;
+    while(true) {
+        active_socket = select(max_sock + 1, readable_sockets, NULL, NULL, NULL);
+        cout << "max_sock: " << max_sock << endl;
+        cout << "active_socket: " << active_socket << endl;
+        if (active_socket < 0) {
+            perror("function select() returned trash");
+            continue;
+        }
 
-    unlink(SOCKET_FILE.c_str());
-    strcpy(server.sun_path, SOCKET_FILE.c_str());
-    if (bind(sock, (struct sockaddr*)&server, sizeof(struct sockaddr_un))) {
-        perror("function bind()");
-        return -1;
-    }
+        if (FD_ISSET(msgsock, readable_sockets)) {
+            msgsock = accept(active_socket, 0, 0);
 
-    cout << "socket_path -->" << server.sun_path << endl;
+            if (msgsock == -1) perror("function accept()");
+            else do {
+                bzero (buf, sizeof(buf));
+                if ((rval = read(msgsock, buf, 1024)) < 0) perror("reading stream mesasge");
+                else if (rval == 0) cout << "ending connection" << endl;
+                else cout << "recived message: " << buf << endl;
+            } while (rval > 0);
 
-    listen(sock, 0);
+        }
 
-    for(;;) {
-        msgsock = accept(sock, 0, 0);
-        if (msgsock == -1) perror("function accept()");
-        else do {
-            bzero (buf, sizeof(buf));
-            if ((rval = read(msgsock, buf, 1024)) < 0) perror("reading stream mesasge");
-            else if (rval == 0) cout << "ending connection" << endl;
-            else cout << "recived message: " << buf << endl;
-        } while (rval > 0);
         close(msgsock);
     }
-    close(sock);
 
-    unlink(SOCKET_FILE.c_str());
+    for (int i = 0; i < max_connections; i++) {
+        close(sockets[i]);
+        unlink(SOCKET_FILES[i].c_str());
+    }
+
+    FD_ZERO(readable_sockets);
+    FD_ZERO(writeable_sockets);
 
     return 0;
 
