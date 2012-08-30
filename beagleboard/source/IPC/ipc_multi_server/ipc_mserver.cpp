9ae835d7ff3560fa12f57cc37e47626fa1769332
Daniel Belles <daniel.belles@web.de>
2012-08-30 13:58:21 +0200

    fixed a bug in project "ipc_multi_server" that the server crashed when the client closed.

--- beagleboard/source/IPC/ipc_multi_server/ipc_mserver.cpp
+++ beagleboard/source/IPC/ipc_multi_server/ipc_mserver.cpp
@@ -75,7 +75,9 @@ int main () {
     while(true) {
         /// check weather new data or new connections were recived
         readable_sockets = all_sockets;
+        cout << "before select()" << endl;
         ready = select(max_sock + 1, readable_sockets, NULL, NULL, NULL);
+        cout << "after select()" << endl;
 
         /// check for a new connection
         if (FD_ISSET(server_sock, readable_sockets)) {
@@ -118,21 +120,17 @@ int main () {
             if (com_sock == -1) continue;
 
             if (FD_ISSET(com_sock, readable_sockets)) {
-                /// read the data from client
-                do {
-                    bzero (buf, sizeof(buf));
-                    if ((rval = read(msgsock, buf, 1024)) < 0) perror("reading stream mesasge");
-                    else if (rval == 0) cout << "ending connection" << endl;
-                    else cout << "recived message: " << buf << endl;
-                } while (rval > 0);
 
-                string str = buf;
-                if (str == "quit") {
+                /// read the data from client
+                bzero (buf, sizeof(buf));
+                if ((rval = read(msgsock, buf, 1024)) < 0) perror("reading stream message");
+                else if (rval == 0) {
                     close(com_sock);
                     FD_CLR(com_sock, all_sockets);
                     client_socks[i] = -1;
                     cout << "client closed communication" << endl;
                 }
+                else cout << "recived message: " << buf << endl;
 
                 /// check for more ready descriptors
                 if (--ready <= 0) break;    //seems not so
@@ -161,71 +159,3 @@ int main () {
     return 0;
 
 }
-
-    /*    addresses[i].sun_family = AF_UNIX;
-        strcpy(addresses[i].sun_path, SOCKET_FILES[i].c_str());
-
-        unlink(SOCKET_FILES[i].c_str());
-
-        sockets[i] = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (sockets[i] < 0) {
-            perror ("function socket()");
-            return -1;
-        }
-
-        if (bind(sockets[i], (struct sockaddr*)&addresses[i], sizeof(struct sockaddr_un)) < 0) {
-            perror("function bind()");
-            return -1;
-        }
-
-        if (listen(sockets[i], 1) == -1) {
-            perror("function listen(): ");
-            return -1;
-        }
-
-        cout << "socket_path -->" << addresses[i].sun_path << "\t sock_num: " << sockets[i] << endl;
-
-        FD_SET(sockets[i], &readable_sockets);
-        if (sockets[i] > max_sock) max_sock = sockets[i];
-    }
-
-    while(true) {
-        active_socket = select(max_sock + 1, &readable_sockets, NULL, NULL, NULL);
-        cout << "max_sock: " << max_sock << endl;
-        cout << "active_socket: " << active_socket << endl;
-        if (active_socket < 0) {
-            perror("function select() returned trash");
-            continue;
-        }
-
-        //if (FD_ISSET(msgsock, &readable_sockets)) {
-            msgsock = accept(active_socket, 0, 0);
-
-            if (msgsock == -1) perror("function accept()");
-            else do {
-                bzero (buf, sizeof(buf));
-                if ((rval = read(msgsock, buf, 1024)) < 0) perror("reading stream mesasge");
-                else if (rval == 0) cout << "ending connection" << endl;
-                else cout << "recived message: " << buf << endl;
-            } while (rval > 0);
-
-            close(msgsock);
-        //}
-
-        int tmp = 0;
-        cin >> tmp;
-
-    }
-
-    for (int i = 0; i < max_connections; i++) {
-        close(sockets[i]);
-        unlink(SOCKET_FILES[i].c_str());
-    }
-
-    FD_ZERO(&readable_sockets);
-    FD_ZERO(&writeable_sockets);
-
-    return 0;
-
-}
-*/
