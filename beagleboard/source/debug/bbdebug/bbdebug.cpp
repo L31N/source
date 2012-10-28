1ce68ee81417e552660b67909d5e1073a108fefd
Daniel Belles <daniel.belles@web.de>
2012-10-28 16:43:18 +0100

    added the debug-api

---
+++ beagleboard/source/debug/bbdebug/bbdebug.cpp
@@ -0,0 +1,24 @@
+// bbdebug.cpp
+
+#include "bbdebug.h"
+
+Debug::Debug(std::string moduleName) {
+    ipcconf = new ipcConfig("../../../etc/");
+    senCon = new ipcSendingConnection(ipcconf->getUDS_FILE_PATH(), ipcconf->getIpcIDToProcessSyn(moduleName), ipcconf->getIpcIDToProcessSyn("DEBUG"));
+}
+
+Debug::~Debug() {
+    delete senCon;
+}
+
+void Debug::send(char* format, ...) {
+    static char buffer[16000];
+    va_list  argptr;
+    va_start( argptr, format );
+    vsprintf( buffer, format, argptr );
+    va_end  ( argptr );
+
+    std::string str(buffer);
+
+    senCon->sendData(str);
+}
