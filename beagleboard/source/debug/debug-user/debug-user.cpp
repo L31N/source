4e3e544e8e60e634908c213bef99618deab80d13
Lukas Metzger <lukas@lukas-netbook.(none)>
2012-10-28 16:50:00 +0100

    beginnig debug server

--- beagleboard/source/debug/debug-user/debug-user.cpp
+++ beagleboard/source/debug/debug-user/debug-user.cpp
@@ -7,19 +7,43 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     //test ob argumente passen
-    if(argc < 20)
+    if(argc < 2)
     {
         cout << "To few parameters!" << endl;
+        return -1;
     }
 
-
+    int *showTable = new int[argc-1];
+    bool showAll = false;
 
     ipcConfig ipcconfig("../../../../../etc/ipc.conf");
 
+    //parameter durchsuchen
+    for(int i = 0; i < argc-1; i++)
+    {
+            string tmp(argv[i+1]);
+
+            if(!tmp.compare("-a"))
+            {
+                showAll=true;
+                delete(showTable);
+                break;
+            }
+            else
+            {
+                //showTable[i]=ipcconfig.get
+            }
+
+            cout << tmp << endl;
+    }
+
+
+
+
     ipcReceivingConnection receivingConnection(ipcconfig.getUDS_FILE_PATH(), ipcconfig.getIpcIDToProcessSyn("DEBUG"), 10);
 
-    while(true)
+    /*while(true)
     {
 
-    }
+    }*/
 }
