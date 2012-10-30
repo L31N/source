fe3ed286553ac0cc62f0190bf18bf1181cf5e9b4
Daniel Belles <daniel.belles@web.de>
2012-10-30 14:29:42 +0100

    modified:   controlscript.sh

--- beagleboard/etc/controlscript.sh
+++ beagleboard/etc/controlscript.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-prozesstest=`/bin/ps -A | /usr/bin/grep ipc_mserver`
-if  [ $prozesstest = `` ]
-then
-          service ipc_mserver start
-fi
-
+while true
+do
+    prozesstest=`/bin/ps -A | /bin/grep ipc_mserver`
+    if  [ $prozesstest = `` ]
+    then
+              service ipc_mserver start
+    fi
+done
 
