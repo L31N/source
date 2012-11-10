edb3ffdbad9bead0e875993725f56a58de00e2f1
Daniel Belles <daniel.belles@web.de>
2012-11-10 14:35:03 +0100

    have written a little bluetooth library libbbbluetooth for more simple bluetooth usage.

---
+++ beagleboard/source/bluetooth/rfcomm/bbbluetooth/bbbluetooth.h
@@ -0,0 +1,52 @@
+// bluetooth_module.h
+
+#ifndef _BLUETOOTH_MODULE_H_
+#define _BLUETOOTH_MODULE_H_
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+
+#include <string>
+#include <iostream>
+
+class BluetoothSocket {
+    public:
+        BluetoothSocket();
+        ~BluetoothSocket();
+
+    protected:
+        struct sockaddr_rc addr;
+        char* buf;
+        int sock;
+};
+
+class BluetoothServerSocket : BluetoothSocket {
+    public:
+        BluetoothServerSocket();
+        ~BluetoothServerSocket();
+
+        bool bt_bind();
+        bool bt_listen();
+        bool bt_accept();
+        std::string bt_recv(void);
+
+    private:
+        struct sockaddr_rc rem_addr;
+        int client_sock;
+        socklen_t slen;
+};
+
+class BluetoothClientSocket : BluetoothSocket {
+    public:
+        BluetoothClientSocket(const std::string destination_device);
+        ~BluetoothClientSocket();
+
+        bool bt_connect();
+        bool bt_send(std::string data);
+
+    private:
+};
+
+#endif // _BLUETOOTH_MODULE_H_
