1a8693b05330822dd74e876a21a6ea7f83afdcb9
l31n <l31n@l31n-notebook.(none)>
2012-11-30 14:39:50 +0100

    added the baudrate settings for the serial_linux.test-project.

--- beagleboard/source/serial_linux/serial_can.cpp
+++ beagleboard/source/serial_linux/serial_can.cpp
@@ -10,6 +10,7 @@
 const unsigned int buffer_size = 1024;
 //const std::string tty_dev = "/dev/ttyS0";
 
+
 int main (int argc, char** argv) {
     if (argc != 3) {
         std::cerr << "wrong number of arguments !!!\nUsage: " << *argv << " [tty_dev] [string to send]\n" << std::endl;
@@ -19,7 +20,17 @@ int main (int argc, char** argv) {
     const std::string tty_dev = *(argv+1);
 
     int tty_sock;
-    char* buffer;
+    //char* buffer;
+
+    struct termios attribs;
+
+    memset(&attribs, 0, sizeof(attribs));
+    attribs.c_iflag=0;
+    attribs.c_oflag=0;
+    attribs.c_cflag=0;
+    attribs.c_lflag=0;
+    attribs.c_cc[VMIN]=1;
+    attribs.c_cc[VTIME]=0;
 
     tty_sock = open(tty_dev.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
     if (tty_sock == -1 )
@@ -28,9 +39,15 @@ int main (int argc, char** argv) {
         return -1;
     }
     else {
+        cfsetospeed(&attribs, B38400);            // 115200 baud
+        cfsetispeed(&attribs, B38400);            // 115200 baud
+
+        tcsetattr(tty_sock, TCSANOW,&attribs);
+
         fcntl(tty_sock, F_SETFL,0);
         std::cout << "Port has been successfully opened, tty_sock_fd: " << tty_sock << std::endl;
     }
+
 
     std::string data_to_send (*(argv+2));
 
