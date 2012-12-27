9042e22f696c5e8c3b033f4958e9f584dbc14a68
Daniel Belles <daniel.belles@web.de>
2012-12-27 23:45:40 +0100

    added a missing 's' in linecount.sh

--- linecount.sh
+++ linecount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 number=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | wc -l)
-echo $number line found
+echo $number lines found
