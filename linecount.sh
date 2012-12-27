44357209ed00a36dae56af3723cec5e725076ac7
Lukas Metzger <loewe.xy@online.de>
2012-12-28 00:02:38 +0100

    Added some functions to linecount.sh

--- linecount.sh
+++ linecount.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-number=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | wc -l)
-echo $number lines found
+numberWith=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | wc -l)
+numberChars=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | wc -m)
+numberMax=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | wc -L)
+numberWithout=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | grep -E .+ | wc -l)
+echo $numberWith lines including newlines found
+echo $numberWithout lines excluded newlines found
+echo $(($numberWith-$numberWithout)) newlines found
+echo $numberChars characters found
+echo The longest line is $numberMax characters long
