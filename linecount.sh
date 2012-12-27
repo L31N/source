73d89839c36b66b09e15bc083e10c733fce8af73
Lukas Metzger <loewe.xy@online.de>
2012-12-28 00:08:08 +0100

    Changed format of linecount.sh

--- linecount.sh
+++ linecount.sh
@@ -4,8 +4,10 @@ numberWith=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c
 numberChars=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | wc -m)
 numberMax=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | wc -L)
 numberWithout=$(find ./ -type f | grep -E -x "(.*\.cpp|.*\.h|.*\.hpp|.*\.conf|.*\.c)" | grep -v "/can-lib/" | xargs -L 1 cat | grep -E .+ | wc -l)
+echo ""
 echo $numberWith lines including newlines found
 echo $numberWithout lines excluded newlines found
 echo $(($numberWith-$numberWithout)) newlines found
 echo $numberChars characters found
 echo The longest line is $numberMax characters long
+echo ""
