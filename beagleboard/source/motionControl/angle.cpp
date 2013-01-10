9ec5472681144974059b982038b63c5ec1402848
Daniel Belles <daniel.belles@web.de>
2013-01-10 19:11:40 +0100

    fixed a bug the Angle - Class of MotionControll.

--- beagleboard/source/motionControl/angle.cpp
+++ beagleboard/source/motionControl/angle.cpp
@@ -4,14 +4,35 @@
 #include "angle.h"
 
 Angle::Angle(unsigned short _angle) { angle = _angle; }
-Angle::Angle(short _angle) { angle = _angle % 360; }
-Angle::Angle(unsigned int _angle) { angle = _angle % 360; }
-Angle::Angle(int _angle) { angle = _angle % 360; }
-Angle::Angle(unsigned long _angle) { angle = _angle % 360; }
-Angle::Angle(long _angle) { angle = _angle % 360; }
 
-Angle::Angle(float _angle) { angle = int(_angle) % 360; }
-Angle::Angle(double _angle) { angle = long(_angle) % 360; }
+Angle::Angle(short _angle) {
+    if (_angle >= 0) angle = _angle % 360;
+    else angle = 360 + _angle % 360;
+}
+
+Angle::Angle(unsigned int _angle) { angle = _angle % 360; }
+
+Angle::Angle(int _angle) {
+    if (_angle >= 0) angle = _angle % 360;
+    else angle = 360 + _angle % 360;
+}
+
+Angle::Angle(unsigned long _angle) { angle = _angle % 360; }
+
+Angle::Angle(long _angle) {
+    if (_angle >= 0) angle = _angle % 360;
+    else angle = 360 + _angle % 360;
+}
+
+Angle::Angle(float _angle) {
+    if (_angle >= 0) angle = int(_angle) % 360;
+    else angle = 360 + int(_angle) % 360;
+}
+
+Angle::Angle(double _angle) {
+    if (_angle >= 0) angle = long(_angle) % 360;
+    else angle = 360 + long(_angle) % 360;
+}
 
 Angle::~Angle() {}
 
