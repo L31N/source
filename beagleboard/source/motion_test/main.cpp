dd22cb9d0a06cbc7916756df0bc794cb3c2d87eb
l31n <l31n@l31n-notebook.(none)>
2013-01-21 12:37:42 +0100

    added negative length for the vectors g and h (motion_test).

--- beagleboard/source/motion_test/main.cpp
+++ beagleboard/source/motion_test/main.cpp
@@ -17,32 +17,48 @@ int main () {
     Vector g;
     Vector h;
 
+    double length_g, length_h;
+    length_g = g.abs();
+    length_h = h.abs();
+
 
     if (m.getAngle() > (315) || m.getAngle() <= 45) {
         g.set(1, 1);
         h.set(-1, 1);
+        length_g = 1;
+        length_h = 1;
     }
     else if (m.getAngle() > 45 && m.getAngle() <= 135) {
         g.set(1, 1);
         h.set(1, -1);
+        length_g = 1;
+        length_h = -1;
     }
     else if (m.getAngle() > 135 && m.getAngle() <= 225) {
         g.set(-1, -1);
         h.set(1, -1);
+        length_g = -1;
+        length_h = -1;
     }
     else if (m.getAngle() > 225 && m.getAngle() <= 315) {
         g.set(-1, -1);
         h.set(-1, 1);
+        length_g = -1;
+        length_h = 1;
     }
     else {
         std::cerr << "wrong vector" << std::endl;
     }
 
-    g.setLenght(cos(m.getAngle(g, false, false)) * m.abs());
-    h.setLenght(sin(m.getAngle(g, false, false)) * m.abs());
+    g.setLenght(cos(abs(m.getAngle(g, false, false))) * m.abs());
+    h.setLenght(sin(abs(m.getAngle(g, false, false))) * m.abs());
 
-    std::cout << "Length(g): " << (int)g.abs() << std::endl;
-    std::cout << "Length(h): " << (int)h.abs() << std::endl;
+
+    length_g *= g.abs();
+    length_h *= h.abs();
+
+    std::cout << "Length(g): " << (int)length_g << std::endl;
+    std::cout << "Length(h): " << (int)length_h << std::endl;
 
     std::cout << "Length(res): " << (int)sqrt((g.abs() * g.abs())+ (h.abs() * h.abs())) << std::endl;
     std::cout << "Length(m): " << (int)m.abs() << std::endl;
