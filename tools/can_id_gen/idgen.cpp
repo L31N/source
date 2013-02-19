5d9d305590c8a6922ff5130ec0afd86ef6ff8161
Daniel Belles <daniel.belles@web.de>
2013-02-20 00:29:36 +0100

    fixed some bugs in can-id-generator added a can-id doku file

--- tools/can_id_gen/idgen.cpp
+++ tools/can_id_gen/idgen.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 
 int main () {
-    unsigned int value;
+    unsigned int value = 0x0F;
+    unsigned int priority;
     unsigned int num;
-    std::cout << "base: "; std::cin >> value;
-    std::cout << "num: "; std::cin >> num;
+    std::cout << "the base used is 0x0F --> 0b001.111*.**** " << std::endl;
+    std::cout << "priority: "; std::cin >> priority;
+    std::cout << "range: "; std::cin >> num;
 
+    value = priority;
     value = (value << 5);
 
     for (int i = 1; i <= num; i++) {
