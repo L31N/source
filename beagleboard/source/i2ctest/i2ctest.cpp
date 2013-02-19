d292b1d70d5b17394236e300e1cbdc206bb38240
bboard1 <soctronic@gmx.de>
2013-02-19 19:15:34 +0100

    the i2c test is working now !

--- beagleboard/source/i2ctest/i2ctest.cpp
+++ beagleboard/source/i2ctest/i2ctest.cpp
@@ -1,11 +1,12 @@
 
 #include <iostream>
 #include <linux/i2c-dev.h>
+#include <fcntl.h>
 #include <inttypes.h>
 
 int main () {
-    int file;
-    if (file = open("/dev/i2c-2", O_RDWR)) {
+    int file = open("/dev/i2c-2", O_RDWR);
+    if (file < 0) {
         std::cerr << "cannot open i2c-device /dev/i2c-2" << std::endl;
         return -1;
     }
@@ -17,17 +18,20 @@ int main () {
     }
 
     unsigned char register_h = 0x02;
-    unsigned char regsiter_l = 0x03;
+    unsigned char register_l = 0x03;
 
     char read_h;
     char read_l;
 
-    read_h = i2c_smbus_read_byte(file, 0x02);
-    read_l = i2c_smbus_read_byte(file, 0x03);
+    //read_h = i2c_smbus_read_byte(file, 0x02);
+    //read_l = i2c_smbus_read_byte(file, 0x03);
+
+    read_h = i2c_smbus_read_byte_data(file, register_h);
+    read_l = i2c_smbus_read_byte_data(file, register_l);
 
     std::cout << "received data: " << std::endl;
-    std::cout << "h_byte: " << read_h << std::endl;
-    std::cout << "l_byte: " << read_l << std::endl;
+    std::cout << "h_byte: " << int(read_h) << std::endl;
+    std::cout << "l_byte: " << int(read_l) << std::endl;
 
     return 0;
 }
