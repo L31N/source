ff27a8c9f0bf20119c6155ba5082e92cba5b568a
Daniel Belles <daniel.belles@web.de>
2013-02-19 23:12:14 +0100

    added debug outputs to sensorAPITest

--- beagleboard/source/sensorAPITest/main.cpp
+++ beagleboard/source/sensorAPITest/main.cpp
@@ -6,8 +6,9 @@
 
 
 int main () {
+    std::cout << "starting programm " << std::endl;
     CmpsSensor* cmps = new CmpsSensor("CMPS", 0x60, "/dev/i2c-2");
-
+    std::cout << "before getAngle()" << std::endl;
     std::cout << "cmps: " << cmps->getAngle() << std::endl;
 
     return 0;
