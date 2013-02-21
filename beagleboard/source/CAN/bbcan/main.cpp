3cf0d8ea791713d8b403bccc9ac79dd983ac512e
Daniel Belles <daniel.belles@web.de>
2013-02-21 23:47:06 +0100

    started to use can_api_test project

--- beagleboard/source/CAN/bbcan/main.cpp
+++ beagleboard/source/CAN/bbcan/main.cpp
@@ -5,16 +5,18 @@
 #include "bbcan.h"
 
 int main () {
-    CAN* can = new CAN("CAN_INTERFACE_TESTING");
-    can->init_member("CAN_TEST_BOARD");
+    CAN* can = new CAN("MOTION_CONTROLLER");
+    can->init_member("LASER_SENSOR2");
 
     sleep(1);
 
     //can->setValue("MOTOR_2", (char*)"hello123");
     while(true) {
         char* buffer;
-        buffer = can->getValue("CAN_TEST_BOARD");
-        if (buffer) std::cout << "data: " << buffer << std::endl;
+        if (can->checkForNewData() {
+            buffer = can->getValue("CAN_TEST_BOARD");
+            std::cout << "data: " << buffer << std::endl;
+        }
     }
 
     sleep(10);
