9590004e1db2d834389077f958d7f2ba82025d15
bboard1 <soctronic@gmx.de>
2013-02-22 01:54:22 +0100

    added some more debug outputs to can_server

--- beagleboard/source/CAN/CANServer/main.cpp
+++ beagleboard/source/CAN/CANServer/main.cpp
@@ -83,7 +83,9 @@ int main () {
 
                 debug.send("data: %s\tsenderID: %i", data.c_str(), senderID);
                 std::cout << "data: " << data << "\tsenderID: " << senderID << std::endl;
-                std::cout << "command: " << data[0] << std::endl;
+                std::cout << "command: " << data[0] << "\t|\t" << int(data[0]) <<  std::endl;
+		std::cout << "data[1]: " << int(data[1]) << std::endl;
+		std::cout << "data[2]: " << int(data[2]) << std::endl;
 
                 char command = data[0];
 
