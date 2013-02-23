958953938c7fe026a5b8b0227d659b6c3717ace7
l31n <l31n@l31n-notebook.(none)>
2013-02-23 04:16:12 +0100

    fixed errors in bbsensor

--- beagleboard/source/sensorAPI/reflexSwitch.cpp
+++ beagleboard/source/sensorAPI/reflexSwitch.cpp
@@ -3,6 +3,6 @@
 
 #include "reflexSwitch.h"
 
-ReflexSwitch::ReflexSwitch(const std::string _icpName, const std::string _canMember) : DigitalSensor (_ipcName, _canMember) {}
+ReflexSwitch::ReflexSwitch(const std::string _ipcName, const std::string _canMember) : DigitalSensor (_ipcName, _canMember) {}
 
 ReflexSwitch::~ReflexSwitch() {}
