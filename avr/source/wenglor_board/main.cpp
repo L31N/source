9ba0173ff52d6f9246b179a2bda375c26ec31a4f
l31n <l31n@l31n-notebook.(none)>
2013-02-23 01:25:52 +0100

    updated the wenglor board software.

--- avr/source/wenglor_board/main.cpp
+++ avr/source/wenglor_board/main.cpp
@@ -10,11 +10,12 @@ int main(void)
 {
 	adc_init();
 
-	can_init(BITRATE_1_MBPS);
+	can_init(BITRATE_100_KBPS);
 
     can_filter_t filter0;
-    filter0.id = 0x020;
-    filter0.mask = 0x7FF;
+    filter0.id = 65;        /// board0
+    filter0.id = 97;        /// board1
+    filter0.mask = 0x7E0;
 
     filter0.flags.rtr = 0;
 
@@ -22,16 +23,42 @@ int main(void)
 
 	while(1)
 	{
-		can_t data_frame;
-        data_frame.flags.rtr = 0;
-        data_frame.id = 0x000;
-        data_frame.length = 3;
+		can_t data_laser_0;
+		can_t data_laser_1;
+		can_t data_reflex;
 
-        data_frame.data[0] = adc_read(0);
-        data_frame.data[1] = adc_read(1);
-        data_frame.data[2] = ( (unsigned long)PINE & (1<<4) ) >> 4;
+		data_laser_0.flags.rtr = 0;
+		data_laser_0.id = 65;
+		data_laser_0.length = 2;
 
-        can_send_message (&data_frame);
+		data_laser_0.flags.rtr = 0;
+		data_laser_0.id = 66;
+		data_laser_0.length = 2;
+
+		data_reflex.flags.rtr = 0;
+		data_reflex.id = 67;
+		data_reflex.length = 1;
+
+		unsigned short adc_laser_0;
+		unsigned short adc_laser_1;
+		bool status_reflex;
+
+		adc_laser_0 = adc_read(0);
+		adc_laser_1 = adc_read(1);
+		status_reflex = ( (unsigned long)PINE & (1<<4) ) >> 4;
+
+        data_laser_0.data[0] = adc_laser_0 & 0x00FF;
+        data_laser_0.data[1] = adc_laser_0 & 0xFF00;
+
+        data_laser_1.data[0] = adc_laser_1 & 0x00FF;
+        data_laser_1.data[1] = adc_laser_1 & 0xFF00;
+
+        data_reflex.data[0] = status_reflex;
+
+
+        can_send_message (&data_laser_0);
+        can_send_message (&data_laser_1);
+        can_send_message (&data_reflex);
 
         _sleep_ms(2);
 	}
