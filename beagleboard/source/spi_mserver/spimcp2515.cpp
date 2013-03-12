a44157442af82c429a17ae12364800b9c7f97d52
daniel.belles <daniel.belles@web.de>
2013-03-12 16:57:07 +0100

    worked on spi_mserver

--- beagleboard/source/spi_mserver/spimcp2515.cpp
+++ beagleboard/source/spi_mserver/spimcp2515.cpp
@@ -5,6 +5,19 @@
 
 SpiMcp2515::SpiMcp2515(const std::string spidev) {
     fd = open(spidev.c_str(), O_RDWR);
+    if (fd <= 0) {
+        std::cerr << "could not open device: " << spidev << std::endl;
+        return;
+    }
 }
 
-SpiMcp2515::~
+SpiMcp2515::~SpiMcp2515() {
+    close(fd);
+}
+
+bool SpiMcp2515::mcp_write_register(char adress, char data) {
+    if (write(fd, &data, 1) != 1) {
+        std::cerr << "spi write error" << std::endl;
+        return false;
+    }
+}
