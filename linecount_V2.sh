1f8523b69a613164bb7456b9480776f563f468c1
Marc.Vieweger <marc.vieweger@vieweger-electronics.de>
2013-11-24 22:17:08 +0100

    added an function to linecount, now linecount_V2.sh

---
+++ linecount_V2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo $#
+string=""
+for input in $@
+do
+	string=$string"|.*\."$input
+done
+
+string=$(echo $string | tail -c +2)
+string="("$string")"
+
+numberWith=$(find ./ -type f | grep -E -x $string | xargs -L 1 cat | wc -l)
+numberChars=$(find ./ -type f | grep -E -x $string | xargs -L 1 cat | wc -m)
+numberWithout=$(find ./ -type f | grep -E -x $string | xargs -L 1 cat | grep -E .+ | wc -l)
+echo ""
+echo $numberWith lines including newlines found
+echo $numberWithout lines excluded newlines found
+echo $(($numberWith-$numberWithout)) newlines found
+echo $numberChars characters found
+echo ""
