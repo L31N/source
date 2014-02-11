2a324bb1ffa9679ac1344b4f1cf8baf5f45b3ed1
Daniel Belles <daniel.belles@web.de>
2014-02-11 22:45:48 +0100

    worked on sensor api IRSEnsor

--- beagleboard/source/sensorAPI/laserSensor.cpp
+++ beagleboard/source/sensorAPI/laserSensor.cpp
@@ -18,26 +18,25 @@ LaserSensor::~LaserSensor() {}
 unsigned int LaserSensor::getDistance(Unit unit) {
 
     can_frame frame;
-    if (can->read(frame)) {
-
+    if (can->read(frame)) { /// on new data
         mmDistance = 0;
         if (number % 2 == 0) for (int i = 0; i < 3; i++) mmDistance |= (frame.data[i] << 8*i);
         else for (int i = 0; i < 3; i++) mmDistance |= (frame.data[i+3] << 8*i);
 
         mmDistance += offset[number];
+    }
 
-        switch (unit) {
-            case mm:
-                debug->send("Distance: %i mm", mmDistance);
-                return mmDistance;
+    switch (unit) {
+        case mm:
+            debug->send("Distance: %i mm", mmDistance);
+            return mmDistance;
 
-            case cm:
-                debug->send("Distance: %i cm", mmDistance/10);
-                return mmDistance/10;
+        case cm:
+            debug->send("Distance: %i cm", mmDistance/10);
+            return mmDistance/10;
 
-            default:
-                debug->send("ERROR: unknown unit: %i", unit);
-                return mmDistance/10;
-        }
+        default:
+            debug->send("ERROR: unknown unit: %i", unit);
+            return mmDistance/10;
     }
 }
